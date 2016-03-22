b1e9af064ebec93203954a0d59a464508bafed77
Rahul P Nath <rahulpnath@gmail.com>
2016-03-22 12:30:44 +1100

    Updated

--- ExplicitAboutDateTime/ExplicitAboutDateTime.Tests/LocationDateTimeTests.cs
+++ ExplicitAboutDateTime/ExplicitAboutDateTime.Tests/LocationDateTimeTests.cs
@@ -5,5 +5,6 @@ namespace ExplicitAboutDateTime.Tests
 {
     public class LocationDateTimeTests
     {
+
     }
 }
--- ExplicitAboutDateTime/ExplicitAboutDateTime/Location.cs
+++ ExplicitAboutDateTime/ExplicitAboutDateTime/Location.cs
@@ -10,7 +10,15 @@ namespace ExplicitAboutDateTime
 
         public Location(string locationCode, string locationName, TimeZoneInfo timeZoneInfo)
         {
-            // Skipped validations
+            if (string.IsNullOrEmpty(LocationCode))
+                throw new ArgumentNullException(nameof(LocationCode));
+
+            if (string.IsNullOrEmpty(locationName))
+                throw new ArgumentNullException(nameof(locationName));
+
+            if (timeZoneInfo == null)
+                throw new ArgumentNullException(nameof(timeZoneInfo));
+
             this.LocationCode = locationCode;
             this.LocationName = locationName;
             this.TimeZoneInfo = timeZoneInfo;
