8c7144c9380d42d51fd11f2cb86665ab90ebe33a
Rahul P Nath <rahulpnath@gmail.com>
2016-06-29 06:22:33 +1000

    Double Writes

---
+++ DoubleWrites/SharedLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+   public  class Class1
+    {
+        public Class1()
+        {
+            JsonConvert.DefaultSettings.EndInvoke(null);
+        }
+    }
+}
