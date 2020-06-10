8a6e4edb922e0afa166b2c69c82a6d8eb868d679
Rahul Nath <rahulpnath@gmail.com>
2020-06-10 12:38:57 +1000

    Sql MSI

---
+++ SqlMSI/SqlMSI/Controllers/QuotesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace SqlMSI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuotesController : ControllerBase
+    {
+        public QuotesController(QuoteContext context)
+        {
+            Context = context;
+        }
+
+        public QuoteContext Context { get; }
+
+        public async Task<IActionResult> Get()
+        {
+            var quotes = await Context.Quote.ToListAsync();
+            return Ok(quotes);
+        }
+    }
+
+    public class Quote
+    {
+        public Guid Id { get; set; }
+        public int QuoteNumber { get; set; }
+    }
+}
---
+++ SqlMSI/SqlMSI/QuoteContext.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Identity;
+using Microsoft.EntityFrameworkCore;
+using SqlMSI.Controllers;
+using System.Data.SqlClient;
+
+namespace SqlMSI
+{
+    public class QuoteContext : DbContext
+    {
+        public QuoteContext(DbContextOptions options) : base(options)
+        {
+            var conn = (Microsoft.Data.SqlClient.SqlConnection)Database.GetDbConnection();
+            var opt = new DefaultAzureCredentialOptions() { ExcludeSharedTokenCacheCredential = true };
+            var credential = new DefaultAzureCredential(opt);
+            var token = credential
+                    .GetToken(new Azure.Core.TokenRequestContext(
+                        new[] { "https://database.windows.net/.default" }));
+            conn.AccessToken = token.Token;
+        }
+
+        public DbSet<Quote> Quote { get; set; }
+    }
+}
